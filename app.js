bee5f0761b6448f28ac6da10ff96fb6d80bcd0c8
Phoenix Work <Work@Phoenixs-MacBook-Pro.local>
2021-02-10 18:13:36 -0800

    finally

--- app.js
+++ app.js
@@ -1,38 +1,28 @@
 function countDown(num) {
-    for (let i = num; i >= 0; i--) {
-        setInterval(function () {
-            if (num != 0)
-            {
-                console.log(num)
-                num--
-            }
-            else if (num === 0)
-            {
-                console.log("DONE!!!")
-            }
-        }, 1000 * i);
-    }
-}
-
-function randomGame() {
-    let num;
-    let result = 0;
     let counter = setInterval(function() {
-        num = Math.random()
-        result++;
-        console.log("Number:", num, "attempt:", result)
-        if (num > .75)
+        if (num <= 0)
         {
-            clearInterval(counter)
-            console.log("It took this many attempts:", result)
+            clearInterval(counter);
+            console.log("Done!");
+        }
+        else
+        {
+            console.log(num);
+            num--;
         }
     }, 1000);
 }
 
-
-    // num = Math.random()
-    // console.log("Number After Loop:", num)
-    // if (num > res)
-    // {
-    //     console.log(i)
-    // }
+function randomGame() {
+    let result = 0;
+    let counter = setInterval(function() {
+        let num = Math.random();
+        result++;
+        console.log("Number:", num, "attempt:", result);
+        if (num > .75)
+        {
+            clearInterval(counter);
+            console.log("It took this many attempts:", result);
+        }
+    }, 1000);
+}
